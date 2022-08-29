4eeb9c0a98c44222c0c8935556985784e90eb91e
ICEJJ <icejj@ICEJJ.local>
2022-08-28 23:31:13 -0400

    test

---
+++ Test/head.h
@@ -0,0 +1,19 @@
+//
+// Created by ICEJJ on 2022/8/24.
+//
+
+#ifndef TEST_HEAD_H
+#define TEST_HEAD_H
+
+#pragma once
+
+#include <iostream>
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+
+//#include <glad/glad.h>
+
+#endif //TEST_HEAD_H
