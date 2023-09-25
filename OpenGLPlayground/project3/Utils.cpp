a909fa008fe46d7f2e9f773bfef29ca73324679c
roy4801 <a82611141@gmail.com>
2023-09-25 21:55:33 +0800

    improve error log

--- OpenGLPlayground/project3/Utils.cpp
+++ OpenGLPlayground/project3/Utils.cpp
@@ -1,51 +1,59 @@
 #include "Utils.h"
+#include "Log.h"
 
-#include <iostream>
 #include <glad/glad.h>
+#include <sstream>
 
 void glDebugOutput(GLenum source, GLenum type, unsigned int id, GLenum severity, GLsizei length, const char *message,
                    const void *userParam)
 {
-// ignore non-significant error/warning codes
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32)
     if(id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
 
-    std::cout << "---------------" << std::endl;
-    std::cout << "Debug message (" << id << "): " <<  message << std::endl;
+    std::stringstream ss;
+    ss << "Debug message (" << id << "): " <<  message << std::endl;
 
     switch (source)
     {
-//        case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
-//        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
-//        case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
-//        case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
-//        case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
-//        case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+        case GL_DEBUG_SOURCE_API:             ss << "Source: API";              break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   ss << "Source: Window System";    break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER: ss << "Source: Shader Compiler";  break;
+        case GL_DEBUG_SOURCE_THIRD_PARTY:     ss << "Source: Third Party";      break;
+        case GL_DEBUG_SOURCE_APPLICATION:     ss << "Source: Application";      break;
+        case GL_DEBUG_SOURCE_OTHER:           ss << "Source: Other";            break;
     }
-    std::cout << std::endl;
+
+    ss << " ";
 
     switch (type)
     {
-//        case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
-//        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
-//        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
-//        case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
-//        case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
-//        case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
-//        case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
-//        case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
-//        case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+        case GL_DEBUG_TYPE_ERROR:               ss << "Type: Error";                break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: ss << "Type: Deprecated Behaviour"; break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  ss << "Type: Undefined Behaviour";  break;
+        case GL_DEBUG_TYPE_PORTABILITY:         ss << "Type: Portability";          break;
+        case GL_DEBUG_TYPE_PERFORMANCE:         ss << "Type: Performance";          break;
+        case GL_DEBUG_TYPE_MARKER:              ss << "Type: Marker";               break;
+        case GL_DEBUG_TYPE_PUSH_GROUP:          ss << "Type: Push Group";           break;
+        case GL_DEBUG_TYPE_POP_GROUP:           ss << "Type: Pop Group";            break;
+        case GL_DEBUG_TYPE_OTHER:               ss << "Type: Other";                break;
     }
-    std::cout << std::endl;
+
+    ss << " ";
 
     switch (severity)
     {
-//        case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
-//        case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
-//        case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
-//        case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+        case GL_DEBUG_SEVERITY_HIGH:
+        case GL_DEBUG_SEVERITY_MEDIUM:
+            ERROR_TAG(TAG_OPENGL, ss.str());
+            break;
+        case GL_DEBUG_SEVERITY_LOW:
+            INFO_TAG(TAG_OPENGL, ss.str());
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+        default:
+            break;
     }
-    std::cout << std::endl;
-    std::cout << std::endl;
+
+#endif
 }
 
 bool ImGui::DragFloat3(const char *label, glm::vec3 *v, float v_speed, float v_min, float v_max, const char *format,
